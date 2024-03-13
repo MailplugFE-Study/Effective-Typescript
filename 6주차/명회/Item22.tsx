d3422c7b219be3e8553ba1e4016f8c722f56ca87
amh6281 <amh6281@naver.com>
2024-03-13 19:05:35 +0900

    feat: Item 22

--- 6주차/명회/Item22.tsx
+++ 6주차/명회/Item22.tsx
@@ -18,3 +18,72 @@ const getUser = (user: UserInfo | AdminInfo) => {
     console.log(user.adminName);
   }
 };
+
+// 2) 분기문 (능숙한 타입 체커)
+const element = document.getElementById("myElement");
+if (element) {
+  element.innerHTML = "hello";
+} else {
+  alert("element not found");
+}
+const element2 = document.getElementById("myElement");
+(element2 as HTMLElement).innerHTML = "hello"; // 안좋은 방법
+
+// 3) 명시적 태그 붙이기
+type UserInfo2 = {
+  type: "user";
+  name: string;
+  id: number;
+};
+type AdminInfo2 = {
+  type: "admin";
+  name: string;
+  id: number;
+};
+type UserOrAdmin = UserInfo2 | AdminInfo2;
+const handleUserOrAdmin = (user: UserOrAdmin) => {
+  switch (user.type) {
+    case "user":
+      console.log(user.name);
+      break;
+    case "admin":
+      console.log(user.name);
+      break;
+  }
+};
+
+// 4) instanceof (클래스의 경우)
+class Car {
+  name = "car";
+  drive() {
+    console.log("drive");
+  }
+}
+
+class House {
+  name = "house";
+  openDoor() {
+    console.log("open door");
+  }
+}
+
+function getInfo(target: Car | House) {
+  if (target instanceof Car) {
+    target.drive();
+    return;
+  }
+
+  target.openDoor();
+  return;
+}
+
+// 5) isArray (배열의 경우)
+type User = {
+  name: string;
+  age: number;
+};
+
+function getUsers(users: User[] | User) {
+  const userList = Array.isArray(users) ? users : [users];
+  return userList;
+}
