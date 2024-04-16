984b140f68684e53a8422eba02edd0f262973a25
khakaa <phr0210@naver.com>
2024-04-16 22:29:43 +0900

    feat: 5-2 완료, 5-1 정리중

--- woowahan-ts/[5장]/5-1.ts
+++ woowahan-ts/[5장]/5-1.ts
@@ -16,3 +16,22 @@ interface Card {
 type PayMethod<T> = T extends "card" ? Card : Bank;
 type CardPayMethodType = PayMethod<"card">;
 type BankPayMethodType = PayMethod<"bank">;
+
+// 2)
+interface PayMethodBaseFromrRes {
+  finacialCode: string;
+  name: string;
+}
+
+interface Bank extends PayMethodBaseFromrRes {
+  fullName: string;
+}
+
+interface Card extends PayMethodBaseFromrRes {
+  appCardType?: string;
+}
+
+type PayMethodInterface = {
+  companyName: string;
+};
+type PayMethodInfo<T extends Bank | Card> = T & PayMethodInterface;
