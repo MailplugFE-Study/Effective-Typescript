011b8363c573c3378d74c369eb50859d7ad88e26
khakaa <phr0210@naver.com>
2024-04-17 23:52:07 +0900

    feat: 하린

--- woowahan-ts/[5장]/5-1.ts
+++ woowahan-ts/[5장]/5-1.ts
@@ -35,3 +35,115 @@ type PayMethodInterface = {
   companyName: string;
 };
 type PayMethodInfo<T extends Bank | Card> = T & PayMethodInterface;
+
+type PayMethodType = PayMethodInfo<Bank> | PayMethodInfo<Card>;
+export const useGetRegisteredList = (
+  type: "card" | "appCard" | "bank"
+): UseQueryResult<PayMethodType[]> => {
+  const url = `/api/${type === "appCard" ? "card" : type}`;
+
+  const fetcher = fetcherFactory<PayMethodType[]>({
+    onSuccess: (res) => {
+      const usablePocketList =
+        res?.filter(
+          (pocket: PocketInfo<Card> | PocketInfo<Bank>) =>
+            pocket?.useType === "USE"
+        ) ?? [];
+      return usablePocketList;
+    },
+  });
+
+  const result = useCommonQuery<PayMethodType[]>(url, undefined, fetcher);
+
+  return result;
+};
+
+const { data } = useGetRegisteredList("card");
+
+// 3)
+type PayMethodType2<T extends "card" | "appCard" | "bank"> = T extends
+  | "card"
+  | "appCard"
+  ? Card
+  : Bank;
+
+export const useGetRegisteredList2 = <T extends "card" | "appCard" | "bank">(
+  type: T
+): UseQueryResult<PayMethodType<T>[]> => {
+  const url = `/api/${type === "appCard" ? "card" : type}`;
+
+  const fetcher = fetcherFactory<PayMethodType<T>[]>({
+    onSuccess: (res) => {
+      const usablePocketList =
+        res?.filter(
+          (pocket: PocketInfo<Card> | PocketInfo<Bank>) =>
+            pocket?.useType === "USE"
+        ) ?? [];
+      return usablePocketList;
+    },
+  });
+
+  const result = useCommonQuery<PayMethodType<T>[]>(url, undefined, fetcher);
+
+  return result;
+};
+
+// 4)
+
+interface ComponentType {
+  (): Element;
+}
+interface RouteBase {
+  name: string;
+  path: string;
+  component: ComponentType;
+}
+
+export interface RouteItem {
+  name: string;
+  path: string;
+  component?: ComponentType;
+  pages?: RouteBase[];
+}
+
+export const routes: RouteItem[] = [
+  {
+    name: "기기 내역 관리",
+    path: "/device-history",
+    component: DeviceHistoryPage,
+  },
+  {
+    name: "헬멧 인증 관리",
+    path: "/helmet-certification",
+    component: HelmetCertificationPage,
+  },
+];
+
+export interface SubMenu {
+  name: string;
+  path: string;
+}
+
+export interface MainMenu {
+  name: string;
+  path?: string;
+  subMenus: SubMenu[];
+}
+
+export type MenuItem = MainMenu | SubMenu;
+export const menuList: MenuItem[] = [
+  {
+    name: "계정 관리",
+    subMenus: [
+      { name: "기기 내역 관리", path: "/device-history" },
+      { name: "헬멧 인증 관리", path: "/helmet-certification" },
+    ],
+  },
+];
+
+type PermissionNames = "기기 정보 관리" | "안전모 인증 관리";
+
+export interface MainMenu2 {
+  //...
+  subMenus?: ReadonlyArray<SubMenu>;
+}
